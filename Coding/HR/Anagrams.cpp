03acf04511f849fc8895b3f189f7b54d8eb9c81a
Surya D <Surya.dheeshjith@gmail.com>
2019-08-09 07:28:11 +0530

    Added

---
+++ Coding/HR/Anagrams.cpp
@@ -0,0 +1,30 @@
+//
+//  Counting valleys.cpp
+//  Coding
+//
+//  Created by Surya Dheeshjith on 05/08/19.
+//  Copyright © 2019 Surya Dheeshjith. All rights reserved.
+
+// Finding all anagrams
+
+
+int sherlockAndAnagrams(string s) {
+
+unordered_map<string,int> mp;
+for(int i=0;i<s.length();i++)
+{
+    for(int j=1;j+i-1<s.length();j++) // Both ways forward = i<j, backward = i>j
+    {
+        string a = s.substr(i,j);
+        sort(a.begin(),a.end());
+        mp[a]++;
+        
+    }
+}
+int count=0;
+for(auto x=mp.begin();x!=mp.end();x++)
+{
+    count+=x->second*(x->second-1)/2;
+}
+return count;
+}
---
+++ Coding/HR/MapsRansom.cpp
@@ -0,0 +1,44 @@
+//
+//  Counting valleys.cpp
+//  Coding
+//
+//  Created by Surya Dheeshjith on 05/08/19.
+//  Copyright © 2019 Surya Dheeshjith. All rights reserved.
+//Find if common words are present in magazine
+
+void checkMagazine(vector<string> magazine, vector<string> note) {
+
+int n=magazine.size(),m=note.size();
+unordered_map<string,int> mp;
+unordered_map<string,int> :: iterator it;
+
+for(int i=0;i<n;i++)
+{
+    it = mp.find(magazine[i]);
+    if(it==mp.end())
+    {
+        mp.insert(pair<string,int>(magazine[i],1));
+    }
+    else
+        mp[magazine[i]]++;
+}
+
+for(int i=0;i<m;i++)
+{
+    it = mp.find(note[i]);
+    if(it==mp.end())
+    {
+        cout<<"No\n";
+        return;
+        }
+        else if(mp[note[i]]==0)
+        {
+            cout<<"No\n";
+            return;
+        }
+        else
+        mp[note[i]]--;
+        }
+        cout<<"Yes\n";
+        
+    }
