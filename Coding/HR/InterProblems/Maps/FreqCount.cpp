c9cfe4ab814d74a12c8748c147de2a8eb6e729f6
Surya D <Surya.dheeshjith@gmail.com>
2019-09-12 07:10:20 +0530

    Added

--- Coding/HR/InterProblems/Maps/FreqCount.cpp
+++ Coding/HR/InterProblems/Maps/FreqCount.cpp
@@ -4,6 +4,11 @@
 //  Created by Surya Dheeshjith on 05/08/19.
 //  Copyright © 2019 Surya Dheeshjith. All rights reserved.
 // Find element with guven frequency efficiently
+//You are given  queries. Each query is of the form two integers described below:
+//- 1 x : Insert x in your data structure.
+//- 2 y : Delete one occurence of y from your data structure, if present.
+//- 3 z : Check if any integer is present whose frequency is exactly z. If yes, print 1 else 0.
+//The queries are given in the form of a 2-D array  of size  where  contains the operation, and contains the data element.
 
 vector<int> freqQuery(vector<vector<int>> queries) {
     
