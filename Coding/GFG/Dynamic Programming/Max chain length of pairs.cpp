49bf023edaef05019c0cfc0e1f1be40e22e9b122
Surya D <Surya.dheeshjith@gmail.com>
2019-09-27 20:15:41 +0530

    Added

---
+++ Coding/GFG/Dynamic Programming/Max chain length of pairs.cpp
@@ -0,0 +1,65 @@
+//
+//  1.cpp
+//  Coding
+//
+//  Created by Surya Dheeshjith on 02/07/19.
+//  Copyright © 2019 Surya Dheeshjith. All rights reserved.
+
+
+//Max chain length of pairs
+
+//DP
+bool compare(val a,val b)
+{
+    if(a.first==b.first)
+    return a.second<b.second;
+    
+    return a.first<b.first;
+}
+
+int maxChainLen(struct val p[],int n)
+{
+//Your code here
+    sort(p,p+n,compare);
+    vector<int> count(n,1);
+    for(int i=1;i<n;i++)
+    {
+        for(int j=0;j<n;j++)
+        {
+            if(p[i].first>p[j].second && count[i]<count[j]+1)
+            count[i]=count[j]+1;
+        }
+    }
+    int max = -1;
+    for(int i=0;i<n;i++)
+    {
+        if(max<count[i])
+        max = count[i];
+        
+    }
+    return max;
+}
+
+//Greedy
+bool customSort (val a, val b) // Different sort. (The least p[].second will definetly be included)
+{
+if(a.second != b.second) return a.second < b.second;
+else return a.first < b.first;
+}
+int maxChainLen(struct val p[],int n)
+{
+if(n == 1 || n==0) return n;
+sort(&p[0],&p[n], customSort);
+
+int end = p[0].second;
+int count = 1;
+for(int i=0; i<n; i++)
+{
+    if(p[i].first> end)
+    {
+count++;
+end = p[i].second;
+}
+}
+return count;
+}
---
+++ Coding/GFG/Dynamic Programming/Minimum operations.cpp
@@ -0,0 +1,66 @@
+//
+//  1.cpp
+//  Coding
+//
+//  Created by Surya Dheeshjith on 02/07/19.
+//  Copyright © 2019 Surya Dheeshjith. All rights reserved.
+
+
+//Min operations to get 0.
+//Divide and Subtract.
+//1. FOR 1 JUST SUBTRACT AND THEN DIVIDE TO GET N-1 BITS; FOR 0 JUST DIVIDE.
+#include<iostream>
+using namespace std;
+int main()
+ {
+    int t;
+    cin>>t;
+    while(t--)
+    {
+        //Enter code here
+        int m;
+        cin>>m;
+        int total=0,set=0;
+        while(m)
+        {
+            if(m%2==1)
+            set++;
+            
+            total++;
+            m=m/2;
+        }
+        cout<<total+set-1<<endl;
+    }
+}
+
+//2.
+
+#include<iostream>
+#include<bits/stdc++.h>
+#define SPEED ios_base::sync_with_stdio(false); cin.tie(NULL); cout.tie(NULL);
+using namespace std;
+int main()
+ {
+     SPEED;
+    int t;
+    cin>>t;
+    while(t--)
+    {
+        //Enter code here
+        int m;
+        cin>>m;
+        vector<int> a(m+1,0);
+        a[1]=1;
+        for(int i=2;i<=m;i++)
+        {
+            if(i%2)
+            a[i]=a[i-1]+1;
+            
+            else
+            {
+                a[i] = a[i/2]+1;
+            }
+        }
+        cout<<a[m]<<endl;
+    }
+}
